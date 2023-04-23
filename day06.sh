45f21e4137b1feeab95af7c160e3946684c5631e
sonu choure <131000134+choure123@users.noreply.github.com>
2023-04-23 21:55:47 +0530

    Add files via upload

---
+++ day06.sh
@@ -0,0 +1,25 @@
+zxZXASQ	xsqa#!/bin/bash
+
+magic_number=$(( (RANDOM % 100) + 1 ))
+
+echo "Think of a number between 1 and 100."
+
+while true; do
+    read -p "Is the number less than $((magic_number/2)) (y/n)? " answer
+    if [[ $answer == "y" ]]; then
+        magic_number=$(( (RANDOM % ((magic_number/2)-1)) + 1 ))
+    elif [[ $answer == "n" ]]; then
+        magic_number=$(( (RANDOM % (100-magic_number)) + magic_number + 1 ))
+    else
+        echo "Invalid input. Please enter y or n."
+        continue
+    fi
+
+    read -p "Is the number $magic_number (y/n)? " answer
+    if [[ $answer == "y" ]]; then
+        echo "Magic number found!"
+        break
+    elif [[ $answer != "n" ]]; then
+        echo "Invalid input. Please enter y or n."
+    fi
+done
---
+++ integers.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Enter the array of integers separated by space:"
+read -a arr
+
+n=${#arr[@]}
+
+found=false
+
+for ((i=0; i<$n-2; i++))
+do
+    for ((j=$i+1; j<$n-1; j++))
+    do
+        for ((k=$j+1; k<$n; k++))
+        do
+            # Check if the sum of the triplet is zero
+            if [ $((arr[$i] + arr[$j] + arr[$k])) -eq 0 ]
+            then
+                echo "Triplet found: ${arr[$i]}, ${arr[$j]}, ${arr[$k]}"
+                found=true
+            fi
+        done
+    done
+done
+
+if [ $found == false ]
+then
+    echo "No triplet found"
+fi
